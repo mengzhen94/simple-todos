a00376c0e738ebb29cefe3b0484f921a2096cd78
Mengzhen <gao.mengzhen94@gmail.com>
2017-02-15 18:14:47 -0500

    step7

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,26 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict} from 'meteor/reactive-dict';
+
 import { Tasks } from '../api/tasks.js';
 
 import './task.js'; 
 import './body.html';
+
+Template.body.onCreated(function bodyOnCreated(){
+	this.state = new ReactiveDict();
+});
  
 Template.body.helpers({
   tasks() {
+  	const instance = Template.instance();
+  	if(instance.state.get('hideCompleted')){
+  		return Tasks.find({checked:{$ne: true}}, {sort:{createAt:-1}});
+  	}
   	return Tasks.find({},{sort:{createAt:-1}});
-  }
+  },
+  incompleteCount(){
+  	return Tasks.find({checked:{$ne: true}}).count();
+  },
 });
 
 Template.body.events({
@@ -28,6 +41,8 @@ Template.body.events({
 		//clear form 
 		target.text.value = '';
 
-		console.log(event);
+	},
+	'change .hide-completed input'(event, instance){
+		instance.state.set('hideCompleted', event.target.checked);
 	},
 });
