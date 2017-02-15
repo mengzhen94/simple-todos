f3f201c0a016bcf29baf54d4897d0eca038df2da
Mengzhen <gao.mengzhen94@gmail.com>
2017-02-15 17:13:49 -0500

    step4

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,6 +5,28 @@ import './body.html';
  
 Template.body.helpers({
   tasks() {
-  	return Tasks.find({});
+  	return Tasks.find({},{sort:{createAt:-1}});
   }
+});
+
+Template.body.events({
+	'submit .new-task'(event){
+		//prevent default browser from submit
+		event.preventDefault();
+
+		//Get value from element
+		const target = event.target;
+		const text = target.text.value;
+
+		//Insert a task into the collection
+		Tasks.insert({
+			text,
+			createAt : new Date(),
+		});
+
+		//clear form 
+		target.text.value = '';
+
+		console.log(event);
+	},
 });
